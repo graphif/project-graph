7af915bdabbf7424bf6e81dfd2cd611965692745
zty012 <z@2y.nz>
2025-10-04 17:21:21 +0800

    chore: 提取出TextureNode

--- app/src/core/sprites/SvgNode.tsx
+++ app/src/core/sprites/SvgNode.tsx
@@ -1,15 +1,11 @@
 import { Dialog } from "@/components/ui/dialog";
 import { serializable } from "@graphif/serializer";
-import { LayoutContainer } from "@pixi/layout/components";
-import { Assets, Color, type ColorSource, ObservablePoint, Point, Sprite } from "pixi.js";
+import { Assets, Color, type ColorSource, Point } from "pixi.js";
 import { Value } from "platejs";
 import { Project } from "../Project";
-import { Entity } from "./abstract/Entity";
-import { StageObject } from "./abstract/StageObject";
+import { TextureNode } from "./TextureNode";
 
-export class SvgNode extends Entity {
-  static RESIZE_HANDLE_SIZE = 16;
-
+export class SvgNode extends TextureNode {
   private _svg: string = "";
   @serializable
   get svg() {
@@ -17,15 +13,17 @@ export class SvgNode extends Entity {
   }
   set svg(value: string) {
     this._svg = value;
-    this.refresh();
-  }
-
-  @serializable
-  get scale(): ObservablePoint {
-    return super.scale;
-  }
-  set scale(value: ObservablePoint) {
-    super.scale = value;
+    Assets.load({
+      src: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(value.replaceAll("currentColor", this.color.toHexa()))}`,
+      data: {
+        resolution: 5,
+        resourceOptions: {
+          scale: 5,
+        },
+      },
+    }).then((texture) => {
+      this.texture = texture;
+    });
   }
 
   private _color: Color = new Color(0xffffff);
@@ -35,10 +33,18 @@ export class SvgNode extends Entity {
   }
   set color(source: ColorSource) {
     this._color = new Color(source);
-    this.refresh();
+    Assets.load({
+      src: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(this.svg.replaceAll("currentColor", new Color(source).toHexa()))}`,
+      data: {
+        resolution: 5,
+        resourceOptions: {
+          scale: 5,
+        },
+      },
+    }).then((texture) => {
+      this.texture = texture;
+    });
   }
-
-  private showResizeHandle = false;
 
   constructor(
     protected readonly project: Project,
@@ -56,101 +62,19 @@ export class SvgNode extends Entity {
       color?: ColorSource;
     },
   ) {
-    super();
+    super(project);
     this.uuid = uuid;
     this.svg = svg;
     this.details = details;
     this.position.copyFrom(position);
     this.color = color;
-
-    let lastClickTime = 0;
-    this.on("click", (e) => {
-      e.stopPropagation();
-      const now = Date.now();
-      if (now - lastClickTime < 300) {
-        // 双击
-        lastClickTime = 0;
-        this.edit();
-      } else {
-        lastClickTime = now;
-        return;
-      }
-    });
   }
 
-  refresh(showResizeHandle = false) {
-    for (const child of this.children) {
-      if (child.label !== StageObject.SELECTION_OUTLINE_LABEL) {
-        child.removeFromParent();
-      }
-    }
-    Assets.load({
-      src: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(this._svg.replaceAll("currentColor", this._color.toHexa()))}`,
-      data: {
-        resolution: 5,
-        resourceOptions: {
-          scale: 5,
-        },
-      },
-    }).then((texture) => {
-      this.addChild(new Sprite(texture));
-
-      if (!showResizeHandle) return;
-      let resizing = false;
-      this.addChild(
-        new LayoutContainer({
-          layout: {
-            width: SvgNode.RESIZE_HANDLE_SIZE,
-            height: SvgNode.RESIZE_HANDLE_SIZE,
-            position: "absolute",
-            left: texture.width - SvgNode.RESIZE_HANDLE_SIZE,
-            top: texture.height - SvgNode.RESIZE_HANDLE_SIZE,
-            // backgroundColor: 0xaaaaff,
-            // borderRadius: SvgNode.RESIZE_HANDLE_SIZE,
-          },
-          cursor: "se-resize",
-          interactive: true,
-        })
-          .on("pointerdown", (e) => {
-            e.stopPropagation();
-            resizing = true;
-          })
-          .on("pointerup", () => {
-            resizing = false;
-          })
-          .on("pointerupoutside", () => {
-            resizing = false;
-          })
-          .on("globalpointermove", (e) => {
-            if (resizing) {
-              const offset = this.project.viewport.toWorld(e.client).subtract(this.position);
-              offset.x = Math.max(offset.x, SvgNode.RESIZE_HANDLE_SIZE + 8);
-              offset.y = Math.max(offset.y, SvgNode.RESIZE_HANDLE_SIZE + 8);
-              if (e.shiftKey) {
-                const ratio = texture.width / texture.height;
-                if (offset.x / offset.y > ratio) {
-                  offset.y = offset.x / ratio;
-                } else {
-                  offset.x = offset.y * ratio;
-                }
-              }
-              this.scale.set(offset.x / texture.width, offset.y / texture.height);
-            }
-          }),
-      );
-    });
-  }
-
-  edit() {
+  override edit() {
     Dialog.input("编辑 SVG 节点", "", { defaultValue: this.svg, multiline: true }).then((result) => {
       if (result) {
         this.svg = result;
       }
     });
-  }
-
-  set selected(value: boolean) {
-    super.selected = value;
-    this.refresh(value);
   }
 }
---
+++ app/src/core/sprites/TextureNode.tsx
@@ -0,0 +1,104 @@
+import { serializable } from "@graphif/serializer";
+import { LayoutContainer } from "@pixi/layout/components";
+import { ObservablePoint, Sprite, Texture } from "pixi.js";
+import { Project } from "../Project";
+import { Entity } from "./abstract/Entity";
+import { StageObject } from "./abstract/StageObject";
+
+export abstract class TextureNode extends Entity {
+  static RESIZE_HANDLE_SIZE = 16;
+
+  private _texture: Texture = Texture.EMPTY;
+  get texture() {
+    return this._texture;
+  }
+  set texture(value: Texture) {
+    this._texture = value;
+    this.refresh();
+  }
+
+  @serializable
+  get scale(): ObservablePoint {
+    return super.scale;
+  }
+  set scale(value: ObservablePoint) {
+    super.scale = value;
+  }
+
+  constructor(protected readonly project: Project) {
+    super();
+    let lastClickTime = 0;
+    this.on("click", (e) => {
+      e.stopPropagation();
+      const now = Date.now();
+      if (now - lastClickTime < 300) {
+        // 双击
+        lastClickTime = 0;
+        this.edit();
+      } else {
+        lastClickTime = now;
+        return;
+      }
+    });
+  }
+
+  private refresh(showResizeHandle = false) {
+    for (const child of this.children) {
+      if (child.label !== StageObject.SELECTION_OUTLINE_LABEL) {
+        child.removeFromParent();
+      }
+    }
+    this.addChild(new Sprite(this.texture));
+
+    if (!showResizeHandle) return;
+    let resizing = false;
+    this.addChild(
+      new LayoutContainer({
+        layout: {
+          width: TextureNode.RESIZE_HANDLE_SIZE,
+          height: TextureNode.RESIZE_HANDLE_SIZE,
+          position: "absolute",
+          left: this.texture.width - TextureNode.RESIZE_HANDLE_SIZE,
+          top: this.texture.height - TextureNode.RESIZE_HANDLE_SIZE,
+          // backgroundColor: 0xaaaaff,
+          // borderRadius: SvgNode.RESIZE_HANDLE_SIZE,
+        },
+        cursor: "se-resize",
+        interactive: true,
+      })
+        .on("pointerdown", (e) => {
+          e.stopPropagation();
+          resizing = true;
+        })
+        .on("pointerup", () => {
+          resizing = false;
+        })
+        .on("pointerupoutside", () => {
+          resizing = false;
+        })
+        .on("globalpointermove", (e) => {
+          if (resizing) {
+            const offset = this.project.viewport.toWorld(e.client).subtract(this.position);
+            offset.x = Math.max(offset.x, TextureNode.RESIZE_HANDLE_SIZE + 8);
+            offset.y = Math.max(offset.y, TextureNode.RESIZE_HANDLE_SIZE + 8);
+            if (!e.shiftKey) {
+              const ratio = this.texture.width / this.texture.height;
+              if (offset.x / offset.y > ratio) {
+                offset.y = offset.x / ratio;
+              } else {
+                offset.x = offset.y * ratio;
+              }
+            }
+            this.scale.set(offset.x / this.texture.width, offset.y / this.texture.height);
+          }
+        }),
+    );
+  }
+
+  abstract edit(): void;
+
+  set selected(value: boolean) {
+    super.selected = value;
+    this.refresh(value);
+  }
+}
