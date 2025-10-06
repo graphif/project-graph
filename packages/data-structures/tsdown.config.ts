324e765ed5ea9d08a03da9e0cb3b92c1d591f62f
zty012 <z@2y.nz>
2025-10-06 20:33:23 +0800

    fix: 修复缺失包的问题

--- packages/data-structures/tsdown.config.ts
+++ packages/data-structures/tsdown.config.ts
@@ -1,4 +1,4 @@
-import originalClassName from "@graphif/unplugin-original-class-name/rollup";
+import originalClassName from "unplugin-original-class-name/rollup";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
