3723a3f102f460e48171464bc002ca4299f36bdd
zty012 <z@2y.nz>
2025-10-06 17:19:24 +0800

    chore: 将unplugin-original-class-name和unplugin-operator-overload从project-graph仓库分离

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest/config" />
 
-import operatorOverload from "@graphif/unplugin-operator-overload/vite";
-import originalClassName from "@graphif/unplugin-original-class-name/vite";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-oxc";
 import path from "node:path";
+import operatorOverload from "unplugin-operator-overload/vite";
+import originalClassName from "unplugin-original-class-name/vite";
 import { createLogger, defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
