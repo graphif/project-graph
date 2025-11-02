3db19c75c0af8d795f9ff0082ad3a5354e477d8c
zty012 <z@2y.nz>
2025-11-02 18:59:45 +0800

    feat: 支持移动Section

--- app/src/core/sprites/Section.tsx
+++ app/src/core/sprites/Section.tsx
@@ -1,5 +1,14 @@
 import { serializable } from "@graphif/serializer";
-import { Color, ColorSource, DestroyOptions, Graphics } from "pixi.js";
+import {
+  Color,
+  ColorSource,
+  Container,
+  DestroyOptions,
+  FederatedPointerEvent,
+  Graphics,
+  Point,
+  PointData,
+} from "pixi.js";
 import { Project } from "../Project";
 import { Association, AssociationMember } from "./abstract/Association";
 import { MyText } from "./MyText";
@@ -10,8 +19,7 @@ export class Section extends Association {
   allowClickToSelect = false;
 
   private _text: string = "";
-  private _cachedTitleText: MyText | null = null;
-  private onTitlePointerDown: (() => void) | null = null;
+  private _cachedTitleContainer: Container | null = null;
 
   @serializable
   get text() {
@@ -19,9 +27,14 @@ export class Section extends Association {
   }
   set text(value: string) {
     this._text = value;
-    this._cachedTitleText = null;
+    this._cachedTitleContainer = null;
     this.refresh();
   }
+
+  private onPointerDownHandler: ((e: FederatedPointerEvent) => void) | null = null;
+  private onPointerUpHandler: ((e: FederatedPointerEvent) => void) | null = null;
+  private onPointerUpOutsideHandler: ((e: FederatedPointerEvent) => void) | null = null;
+  private onGlobalPointerMoveHandler: ((e: FederatedPointerEvent) => void) | null = null;
 
   constructor(
     protected readonly project: Project,
@@ -29,7 +42,7 @@ export class Section extends Association {
       uuid = crypto.randomUUID() as string,
       color = 0xffffff,
       members = [],
-      text = "test",
+      text = "test123",
     }: {
       uuid?: string;
       color?: ColorSource;
@@ -43,6 +56,40 @@ export class Section extends Association {
     this.members = members;
     this.text = text;
     this.refresh();
+
+    let moving = false;
+    const lastPos = new Point();
+    this.onPointerDownHandler = (e) => {
+      console.log(1);
+      if (e.button !== 0) return;
+      moving = true;
+      const pos = this.project.viewport.toWorld(e.client);
+      lastPos.copyFrom(pos);
+    };
+    this.onPointerUpHandler = () => {
+      moving = false;
+    };
+    this.onPointerUpOutsideHandler = () => {
+      moving = false;
+    };
+    this.onGlobalPointerMoveHandler = (e) => {
+      if (moving) {
+        const pos = this.project.viewport.toWorld(e.client);
+        const movementX = pos.x - lastPos.x;
+        const movementY = pos.y - lastPos.y;
+        lastPos.copyFrom(pos);
+        // 移动所有members
+        for (const member of this.members) {
+          member.entity.position.x += movementX;
+          member.entity.position.y += movementY;
+        }
+        this.emit("update");
+      }
+    };
+    this.on("pointerdown", this.onPointerDownHandler)
+      .on("pointerup", this.onPointerUpHandler)
+      .on("pointerupoutside", this.onPointerUpOutsideHandler)
+      .on("globalpointermove", this.onGlobalPointerMoveHandler);
   }
 
   get source() {
@@ -61,20 +108,29 @@ export class Section extends Association {
     this.refresh();
   }
 
-  private getOrCreateTitleText(x: number, y: number): MyText {
-    if (!this._cachedTitleText) {
-      this.onTitlePointerDown = () => {
-        this.selected = true;
-      };
-      this._cachedTitleText = new MyText(this._text, {
-        style: {
-          fontSize: Section.TITLE_SIZE,
-        },
-        interactive: true,
-      }).on("pointerdown", this.onTitlePointerDown);
+  private getOrCreateTitleText(x: number, y: number): Container {
+    if (!this._cachedTitleContainer) {
+      this._cachedTitleContainer = new Container({
+        layout: {},
+      });
+      this._cachedTitleContainer.addChild(
+        new MyText(this._text, {
+          style: {
+            fontSize: Section.TITLE_SIZE,
+          },
+          interactive: true,
+          layout: true,
+        }),
+      );
     }
-    this._cachedTitleText.position.set(x, y);
-    return this._cachedTitleText;
+    this._cachedTitleContainer.position.set(x, y);
+    const bounds = this.members.map((m) => m.entity.getWorldBounds());
+    const minX = Math.min(...bounds.map((e) => e.x)) - this.position.x;
+    const maxX = Math.max(...bounds.map((e) => e.x + e.width)) - this.position.x;
+    this._cachedTitleContainer.layout = {
+      width: maxX - minX,
+    };
+    return this._cachedTitleContainer;
   }
 
   refresh() {
@@ -99,14 +155,64 @@ export class Section extends Association {
 
   override destroy(options?: DestroyOptions): void {
     // 移除缓存的标题文本的事件监听器
-    if (this._cachedTitleText && this.onTitlePointerDown) {
-      this._cachedTitleText.off("pointerdown", this.onTitlePointerDown);
+    if (this._cachedTitleContainer) {
+      this._cachedTitleContainer.destroy();
+      this._cachedTitleContainer = null;
     }
-    if (this._cachedTitleText) {
-      this._cachedTitleText.destroy();
-      this._cachedTitleText = null;
-    }
-    this.onTitlePointerDown = null;
+
+    if (this.onPointerDownHandler) this.off("pointerdown", this.onPointerDownHandler);
+    if (this.onPointerUpHandler) this.off("pointerup", this.onPointerUpHandler);
+    if (this.onPointerUpOutsideHandler) this.off("pointerupoutside", this.onPointerUpOutsideHandler);
+    if (this.onGlobalPointerMoveHandler) this.off("globalpointermove", this.onGlobalPointerMoveHandler);
+    this.onPointerDownHandler = null;
+    this.onPointerUpHandler = null;
+    this.onPointerUpOutsideHandler = null;
+    this.onGlobalPointerMoveHandler = null;
+
     super.destroy(options);
+  }
+
+  override myContainsPoint(point: PointData): boolean {
+    const bounds = this.getWorldBounds();
+    const titleHeight = this.text ? Section.TITLE_SIZE + Section.PADDING : 0;
+    const strokeWidth = 2;
+    // prettier-ignore
+    return (
+      // 标题区域
+      (
+        point.x >= bounds.x &&
+        point.x <= bounds.maxX &&
+        point.y >= bounds.y &&
+        point.y <= bounds.y + titleHeight
+      ) ||
+      // 左边
+      (
+        point.x >= bounds.x &&
+        point.x <= bounds.x + strokeWidth &&
+        point.y >= bounds.y &&
+        point.y <= bounds.maxY
+      ) ||
+      // 右边
+      (
+        point.x >= bounds.maxX - strokeWidth &&
+        point.x <= bounds.maxX &&
+        point.y >= bounds.y &&
+        point.y <= bounds.maxY
+      ) ||
+      // 上边
+      (
+        point.x >= bounds.x &&
+        point.x <= bounds.maxX &&
+        point.y >= bounds.y &&
+        point.y <= bounds.y + strokeWidth
+      ) ||
+      // 下边
+      (
+        point.x >= bounds.x &&
+        point.x <= bounds.maxX &&
+        point.y >= bounds.maxY - strokeWidth &&
+        point.y <= bounds.maxY
+      )
+    );
   }
 }
