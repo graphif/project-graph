e81ad536900a117313176bf4c461ad3bf565216c
zty012 <z@2y.nz>
2025-11-02 18:01:01 +0800

    fix: 修复Section的选中框位置不正确

--- app/src/core/sprites/abstract/StageObject.tsx
+++ app/src/core/sprites/abstract/StageObject.tsx
@@ -28,11 +28,12 @@ export abstract class StageObject extends LayoutContainer {
         label: StageObject.SELECTION_OUTLINE_LABEL + this.uuid,
       });
       // 在自己的bounds外面一圈画框
+      const bounds = this.getWorldBounds();
       g.roundRect(
-        this.x - StageObject.SELECTION_OUTLINE_PADDING,
-        this.y - StageObject.SELECTION_OUTLINE_PADDING,
-        this.width / this.scale.x + StageObject.SELECTION_OUTLINE_PADDING * 2,
-        this.height / this.scale.y + StageObject.SELECTION_OUTLINE_PADDING * 2,
+        bounds.x - StageObject.SELECTION_OUTLINE_PADDING,
+        bounds.y - StageObject.SELECTION_OUTLINE_PADDING,
+        bounds.width / this.scale.x + StageObject.SELECTION_OUTLINE_PADDING * 2,
+        bounds.height / this.scale.y + StageObject.SELECTION_OUTLINE_PADDING * 2,
         StageObject.SELECTION_OUTLINE_PADDING + (this.layout?._styles?.custom.borderRadius ?? 8),
       );
       g.stroke({
