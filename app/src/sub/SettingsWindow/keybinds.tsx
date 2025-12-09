91f4220a8fa48e8c26585d24e6526b7701a5a639
阿岳 <ayue@fittentech.com>
2025-12-10 00:18:32 +0800

    docs: 补充翻译

--- app/src/sub/SettingsWindow/keybinds.tsx
+++ app/src/sub/SettingsWindow/keybinds.tsx
@@ -320,6 +320,7 @@ const shortcutKeysGroups: ShortcutKeysGroup[] = [
       "clickStartFilePanelButton",
       "switchActiveProject",
       "switchActiveProjectReversed",
+      "switchStealthMode",
     ],
   },
   {
