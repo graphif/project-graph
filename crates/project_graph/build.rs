297143987f4ff05dbfd949f3ceac3489f2223b5b
zty012 <z@2y.nz>
2026-02-14 21:43:29 +0800

    chore: 添加linux,windows,macos的cfg_alias

--- crates/project_graph/build.rs
+++ crates/project_graph/build.rs
@@ -5,9 +5,15 @@ fn main() {
         android: { target_os = "android" },
         wasm: { target_arch = "wasm32" },
         desktop: { all(not(target_os = "android"), not(target_arch = "wasm32")) },
+        linux: { target_os = "linux" },
+        windows: { target_os = "windows" },
+        macos: { target_os = "macos" },
     }
 
     println!("cargo::rustc-check-cfg=cfg(android)");
     println!("cargo::rustc-check-cfg=cfg(wasm)");
     println!("cargo::rustc-check-cfg=cfg(desktop)");
+    println!("cargo::rustc-check-cfg=cfg(linux)");
+    println!("cargo::rustc-check-cfg=cfg(windows)");
+    println!("cargo::rustc-check-cfg=cfg(macos)");
 }
