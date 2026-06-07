4eb238bbdec2af116016cce8cae1facf33c82e7b
zty012 <z@2y.nz>
2026-06-07 13:04:55 +0800

    chore: ci

---
+++ .github/scripts/set-aha-features.mjs
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+import { readFileSync, writeFileSync } from "fs";
+
+const features = (process.argv[2] ?? "").split(",").filter(Boolean);
+
+const CARGO_TOML_PATH = "app/src-tauri/Cargo.toml";
+
+const conf = readFileSync(CARGO_TOML_PATH);
+const updated = conf
+  .toString()
+  .replace(
+    /aha = { version = "(.*)", features = \["(.*)"\] }/,
+    `aha = { version = "$1", features = ${JSON.stringify(features)} }`,
+  );
+
+writeFileSync(CARGO_TOML_PATH, updated);
+
+console.log(updated);
