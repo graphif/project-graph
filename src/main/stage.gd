864e0054539d44ee5f1dce867e8cc3d275951f0e
zty012 <z@2y.nz>
2026-08-12 16:00:17 +0800

    feat: 右键菜单

--- src/main/stage.gd
+++ src/main/stage.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const TEXT_NODE = preload("uid://btnefrbc5lowu")
+@onready var popup_menu: PopupMenu = $PopupMenu
 
 
 func _ready() -> void:
@@ -19,3 +20,18 @@ func _set_owner_recursive(node: Node, new_owner: Node) -> void:
 	node.owner = new_owner
 	for child in node.get_children():
 		_set_owner_recursive(child, new_owner)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.double_click:
+			var node: TextNode = TEXT_NODE.instantiate()
+			node.text = "..."
+			node.position = get_global_mouse_position()
+			print(event.position)
+			add_child(node)
+			get_viewport().set_input_as_handled()
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			popup_menu.position = event.position
+			popup_menu.popup()
+			get_viewport().set_input_as_handled()
--- src/stage_object/entity/text_node/text_node.gd
+++ src/stage_object/entity/text_node/text_node.gd
@@ -10,6 +10,9 @@ extends Entity
 		if is_node_ready():
 			label.text = value
 
+var is_dragging: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,10 +21,29 @@ func _ready() -> void:
 
 
 func _gui_input(event: InputEvent) -> void:
+	# 进入编辑模式
 	if label.visible and event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.double_click:
 			enter_edit_mode()
 			get_viewport().set_input_as_handled()
+			return
+	# 开始拖拽
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				is_dragging = true
+				# 记录鼠标点击位置相对于节点左上角的偏移量，防止节点瞬间“跳”到鼠标中心
+				drag_offset = get_local_mouse_position()
+			else:
+				is_dragging = false
+			# edge case: 在编辑状态下拖拽另一个节点，导致无法退出编辑状态，所以这里不调用set_input_as_handled
+			return
+	# 拖拽
+	if event is InputEventMouseMotion and is_dragging:
+		# 将全局鼠标位置转换为父节点坐标系，减去偏移量得到新位置
+		global_position = get_global_mouse_position() - drag_offset
+		get_viewport().set_input_as_handled()
+		return
 
 
 func _unhandled_input(event: InputEvent) -> void:
