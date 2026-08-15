5660b2adeae7aadd04785d8ca93641699188f265
zty012 <z@2y.nz>
2026-08-15 22:56:06 +0800

    feat: 使用显示器刷新率作为TPS

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,1 +1,6 @@
 extends Control
+
+
+func _ready() -> void:
+	var screen_fps := DisplayServer.screen_get_refresh_rate(DisplayServer.SCREEN_OF_MAIN_WINDOW)
+	Engine.physics_ticks_per_second = screen_fps
